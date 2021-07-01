aea2333a94c038731c008c000677f2212835939a
ZakAdam <adam.hrac@gmail.com>
2021-07-01 13:49:06 +0200

    added second version

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,7 +5,28 @@ class ArticlesController < ApplicationController
     render json: articles.to_json
   end
 
-  def create
-    puts(params[:body])
+  def choose_one
+    chosen = Article.find(params[:id])
+    render json: chosen.to_json
   end
+
+  def create
+    puts(params[:article])
+    puts(params[:article][:title])
+    puts(params[:article][:body])
+    if article_params
+      puts("LETSGOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO")
+    else
+      puts("ANI NIEEEEEEEEEEEEEEEEEEEEEEEE :(((((((())))))))")
+    end
+    new_article = Article.new(title: params[:article][:title], body: params[:article][:body])
+    new_article.save
+  end
+
+  private
+    def article_params
+      #params.require(:article).permit(:title, :body)
+      control = params.require(:article).permit(:title, :body)
+      puts(control)
+    end
 end
